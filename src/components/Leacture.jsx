0cce195bf455186768717818219e8eb517eac556
Sagar Nirwal <62455271+sn247776@users.noreply.github.com>
2023-05-22 16:03:56 +0530

    problume for server

--- src/components/Leacture.jsx
+++ src/components/Leacture.jsx
@@ -1,12 +1,13 @@
 import { Box } from '@mui/material'
 import React from 'react'
+import ImgLec from "../assets/course-grid.webp"
 import "./leacture.css"
 
 function Leacture(props) {
   return (
     <Box className="leacture-box">
         <Box>
-            <img src={props.poster} alt='lect-img'/>
+        <img src={props.poster || ImgLec} alt='lect-img' />
         </Box>
         <Box>
             <h3>#{props.index} {props.title}</h3>
